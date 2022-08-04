f7dab0e12ab0198e2c8ac76a802bec3ea4e9667b
Pretelt Juan <preteltjuanoficial@gmail.com>
2022-08-04 11:49:30 -0500

    Se adiciona about page

--- movie/views.py
+++ movie/views.py
@@ -2,4 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def home(request):
-    return render(request, "home.html", {"name": "Juan Pretelt"})
+    return render(request, "home.html", {"screenName": "Home"})
+
+def about(request):
+    return render(request, "about.html", {"screenName": "About"})
